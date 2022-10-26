d21a476837b602a8f5c83f3ca143d3c71bb3caf5
Ararageee <dc.gonzales0332@student.tsu.edu.ph>
2022-10-26 22:24:49 +0800

    Files Commited

---
+++ backend/schemas/nutrients.js
@@ -0,0 +1,77 @@
+export default {
+      name: 'nutrients',
+      title: 'Nutrients',
+      type: 'document',
+      fields: [
+   {
+      name: 'calories',
+      title: 'Calories',
+      type: 'number',
+    },
+    {
+      name: 'totalfat',
+      title: 'Total Fat',
+      type: 'number',
+    },
+    {
+      name: 'saturatedfat',
+      title: 'Saturated Fat',
+      type: 'number',
+    },
+    {
+      name: 'transfat',
+      title: 'TransFat',
+      type: 'number',
+    },
+    {
+      name: 'cholesterol',
+      title: 'Cholesterol',
+      type: 'number',
+    },
+    {
+      name: 'sodium',
+      title: 'Sodium',
+      type: 'number',
+    },
+    {
+      name: 'totalcarb',
+      title: 'Total Carbohydrates',
+      type: 'number',
+    },
+    {
+      name: 'dietaryFiber',
+      title: 'Dietary Fiber',
+      type: 'number',
+    },
+    {
+      name: 'sugar',
+      title: 'Sugar',
+      type: 'number',
+    },
+    {
+      name: 'protein',
+      title: 'Protein',
+      type: 'number',
+    },
+    {
+      name: 'vitaminA',
+      title: 'Vitamin A',
+      type: 'number',
+    },
+    {
+      name: 'vitaminC',
+      title: 'Vitamin C',
+      type: 'number',
+    },
+    {
+      name: 'calcium',
+      title: 'Calcium',
+      type: 'number',
+    },
+    {
+      name: 'iron',
+      title: 'Iron',
+      type: 'number',
+    },
+      ]
+};
