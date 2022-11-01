8995075f24c159445753e7f321457875e0461405
jokwa <joshuasarmiento0021@gmail.com>
2022-11-02 02:24:50 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,9 +13,10 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// eto na si jok
+// eto na si jok PART 2
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
+
