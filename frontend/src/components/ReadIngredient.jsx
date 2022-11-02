c22436c05851b6e96c6e13b53c2b900d91224979
NLM0 <n.manalad0204@student.tsu.edu.ph>
2022-11-03 04:13:11 +0800

    Nioks: Fixed Table on Admin

--- frontend/src/components/ReadIngredient.jsx
+++ frontend/src/components/ReadIngredient.jsx
@@ -25,22 +25,22 @@ const ReadIngredient = () => {
     useEffect(() => {
         if (searchIngredientTerm !== '') {
 
-          const query = searchIngredientQuery(searchIngredientTerm.toLowerCase());
-          client.fetch(query).then((data) => {
-            setIngredientList(data);
-            console.log(ingredientList);
+            const query = searchIngredientQuery(searchIngredientTerm.toLowerCase());
+            client.fetch(query).then((data) => {
+                setIngredientList(data);
+                console.log(ingredientList);
 
-            
-          });
-        } 
-        else {
-          client.fetch(allIngredientsQuery).then((data) => {
-            setIngredientList(data);
 
-            console.log(ingredientList);
-          });
+            });
         }
-      }, [searchIngredientTerm]);
+        else {
+            client.fetch(allIngredientsQuery).then((data) => {
+                setIngredientList(data);
+
+                console.log(ingredientList);
+            });
+        }
+    }, [searchIngredientTerm]);
 
 
 
@@ -58,31 +58,75 @@ const ReadIngredient = () => {
             />
 
             {/* DISPLAY INGREDIENTS */}
-            {ingredientList?.map((item, index) => (
-                <div key={index}>
-                    <h1>IngAdminName: {item?.ingAdminName}</h1>
-                    {item?.baseSize?.map((c) => (
-                        <div key={c?._key}>
-                            <h3>baseSizeNum: {c.baseSizeNum}</h3>
-                            <h3>calcium: {c.calcium}</h3>
-                            <h3>calories: {c.calories}</h3>
-                            <h3>cholesterol: {c.cholesterol}</h3>
-                            <h3>dietaryFiber: {c.dietaryFiber}</h3>
-                            <h3>iron: {c.iron}</h3>
-                            <h3>protein: {c.protein}</h3>
-                            <h3>saturatedfat: {c.saturatedfat}</h3>
-                            <h3>sodium: {c.sodium}</h3>
-                            <h3>sugar: {c.sugar}</h3>
-                            <h3>totalfat: {c.totalfat}</h3>
-                            <h3>transfat: {c.transfat}</h3>
-                            <h3>vitaminA: {c.vitaminA}</h3>
-                            <hr />
-                        </div>
-                    ))}
-                </div>
-            ))}
+            <div className="flex flex-col">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 inline-block  sm:px-6 lg:px-8">
+                        <div className="overflow-hidden">
+                            <table className='w-full text-center border border-slate-300 border-fixed'>
 
-        
+                                {ingredientList?.map((item, index) => (
+                                    <div key={index}>
+                                        <thead className="border-b w-full">
+                                            <tr className="bg-white border-b ">
+
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 w-full ">{item?.ingAdminName}</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Base Size </th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Calcium</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Calories</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Cholesterol</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Dietary Fiber</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Iron</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Protien</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Saturated Fat</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Sodium</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Sugar</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">TotalFat</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">TransFat</th>
+                                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">Vitamin A</th>
+                                            </tr>
+                                        </thead>
+
+                                        {item?.baseSize?.map((c) => (
+                                            <div key={c?._key}>
+
+                                            
+
+                                                <tbody className="border-b">
+                                                    
+                                                    <tr className="bg-white border-b w-full">
+                                                        <td scope="col" className="w-56 text-sm font-medium text-gray-900 px-6 py-4"></td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.baseSizeNum}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4 ">{c.calcium}</td>
+                                                        <td scope="col" className="w-24 ext-sm font-medium text-gray-900 px-6 py-4">{c.calories}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.cholesterol}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.dietaryFiber}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.iron}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.protein}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.saturatedfat}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.sodium}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.sugar}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4">{c.totalfat}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4" >{c.transfat}</td>
+                                                        <td scope="col" className="w-24 text-sm font-medium text-gray-900 px-6 py-4" >{c.vitaminA}</td>
+                                                    </tr>
+                                                </tbody>
+                                               
+
+
+                                            </div>
+                                        )
+                                        )}
+                                    </div>))}
+
+
+                            </table>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
