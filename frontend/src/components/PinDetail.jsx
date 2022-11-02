09e9ece04a64af2ec7b2bd8d27ddfb5a9b9d3914
Goldenboi_ <111736769+Araragee@users.noreply.github.com>
2022-11-02 11:18:58 +0800

    Update PinDetail.jsx

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -8,7 +8,7 @@ import { v4 as uuidv4 } from 'uuid';
 import ReactTooltip from 'react-tooltip';
 import { client, urlFor } from '../client';
 import MasonryLayout from './MasonryLayout';
-import { pinDetailMorePinQuery, pinDetailQuery, ingredientBaseValue } from '../utils/data';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
 import Spinner from './Spinner';
 
 const PinDetail = ({ user }) => {
@@ -19,8 +19,6 @@ const PinDetail = ({ user }) => {
   const [addingComment, setAddingComment] = useState(false);
   const [savingPost, setSavingPost] = useState(false);
   const [ingredient, setIngredient] = useState();
-  const [baseval, setBaseval] = useState(['']);
-
   const navigate = useNavigate();
 
   const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
@@ -28,7 +26,6 @@ const PinDetail = ({ user }) => {
   let alreadySaved = pinDetail?.save?.filter((item) => item?.postedBy?._id === User?.sub);
 
   alreadySaved = alreadySaved?.length > 0 ? alreadySaved : [];
-
 
   const fetchPinDetails = () => {
     const query = pinDetailQuery(pinId);
@@ -108,20 +105,6 @@ const PinDetail = ({ user }) => {
     fetchPinDetails();
   }, [pinId]);
 
-
-  // useEffect(() => {
-  //   fetchIngredientBaseValue()
-  // }, [baseVal]);
-
-   const fetchIngredientBaseValue = () => {
-    const query = ingredientBaseValue(pinDetail.ingredient);
-    if (query) {
-      client.fetch(`${query}`)
-      .then((data) => {
-        setBaseval(data);
-        {console.log(data)}
-      })}}
-      
   const addComment = () => {
     if (comment) {
       setAddingComment(true);
@@ -145,7 +128,6 @@ const PinDetail = ({ user }) => {
     );
   }
 
-  
   return (
     <>
       {pinDetail && (
@@ -156,6 +138,7 @@ const PinDetail = ({ user }) => {
               src={(pinDetail?.image && urlFor(pinDetail?.image).url())}
               alt="user-post"
             />
+
           </div>
 
           <div className="w-full p-5 flex-1 xl:min-w-620">
@@ -207,24 +190,13 @@ const PinDetail = ({ user }) => {
               </p>
              
               <div className="flex justify-left items-left flex-col border bg-gray-100">
-              <div className="float-root  flex">
-              <div className='float-left py-4'>
+              <div class="float-root  flex">
+              <div class='float-left py-4'>
+              <p style={{ marginBottom: '15px' }} class="font-semibold "> Ingredients: </p>
+              {pinDetail.ingredient.map((item) => (
+                <div style={{ width: 'auto', height: 'auto', marginLeft: '10px', position: 'relative' }}>
 
-              <p style={{ marginBottom: '15px' }} className="font-semibold "> Ingredients: </p>
-              {pinDetail.ingredient.map((item, i) => (
-                <div style={{ width: 'auto', height: 'auto', marginLeft: '10px', position: 'relative' }} key={i}>
-                <li className="capitalize flex flex-nowrap" key={i}>{item}</li>
-
-                </div>
-              ))}
-                </div>
-
-                <div className='float-middle py-4 ml-8'>
-              <p style={{ marginBottom: '15px' }} className="font-semibold"> Metric </p>
-              {pinDetail.metric.map((item, i) => (
-                <div style={{ width: 'auto', height: 'auto', marginLeft: '10px', position: 'relative' }} key={i}>
-                  <li className=" flex flex-nowrap" key={i}>{item}</li>
-                  
+                <li class="capitalize flex flex-nowrap" key="{item}">{item}</li>
                 </div>
               ))}
                 </div>
@@ -246,10 +218,10 @@ const PinDetail = ({ user }) => {
               ))}
                  </div>
                  </div>
-              <p style={{ marginBottom: '15px' }} className="font-semibold"> Procedure: </p>
-              {pinDetail.procedure.map((item, i) => (
-                <div style={{ width: 'auto', height: 'auto', marginLeft: '10px', position: 'relative' }} key={i}>
-                  <li className="capitalize" key={i}>{item}</li>
+              <p style={{ marginBottom: '15px' }} class="font-semibold"> Procedure: </p>
+              {pinDetail.procedure.map((item) => (
+                <div style={{ width: 'auto', height: 'auto', marginLeft: '10px', position: 'relative' }}>
+                  <li class="capitalize" key="{item}">{item}</li>
                 </div>
               ))}
 
@@ -262,13 +234,15 @@ const PinDetail = ({ user }) => {
             </Link>
             <h2 className="mt-5 text-2xl"> Comments </h2>
             <div className="max-h-370 overflow-y-auto">
-              {pinDetail?.comments?.map((comment, i) => (
-                <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={i}>
+              {pinDetail?.comments?.map((comment) => (
+                <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={comment.comment}>
+
                   <img
                     src={comment.postedBy?.image}
                     alt="user-profile"
                     className="pointer-events-none w-10 h-10 rounded-full cursor-pointer"
                   />
+
                   <div className="flex flex-col">
                     <p className="font-bold">{comment.postedBy?.userName}</p>
                     <p>{comment.comment}</p>
