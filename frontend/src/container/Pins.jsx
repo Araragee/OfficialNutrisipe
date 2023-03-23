33dd71ba70ce132b3251b7ccc5c0fbd4e43b3693
Goldenboi_ <dc.gonzales0332@student.tsu.edu.ph>
2023-03-23 12:55:09 +0800

    Fixed bugs on follow and search

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -15,7 +15,6 @@ const Pins = ({ user }) => {
   //   socket?.emit("newUser", )
   // }, [socket, user])
   
-  console.log(user)
   return (
     <div className="px-2 md:px-5">
       <div className="bg-gray-50">
